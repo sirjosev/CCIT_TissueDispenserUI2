b2fdcf0a2be83b40ee3e60d955b2430900a82a93
sirjosephholmes <sirjosephholmes@gmail.com>
2024-06-09 16:03:03 +0000

    ada animasinya dan data list ditambahin nya dari scrip

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Add JS here
+// Add event listener for click events on list items
+$(".tissue-list").on("click", function() {
+    $(this).addClass("animated bounce");
+
+    // Remove animation after 1 second
+    setTimeout(function() {
+        $(".tissue-list").removeClass("animated bounce");
+    }, 1000);
+});
